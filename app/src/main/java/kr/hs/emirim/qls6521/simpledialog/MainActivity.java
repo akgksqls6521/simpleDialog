d51a1ea914d9606d44bfe20c22802ad50f735c20
Hanbin Ma <qls6521@naver.com>
2016-06-07 20:46:42 +0900

    #4 change List Box

--- app/src/main/java/kr/hs/emirim/qls6521/simpledialog/MainActivity.java
+++ app/src/main/java/kr/hs/emirim/qls6521/simpledialog/MainActivity.java
@@ -8,22 +8,29 @@ import android.view.View;
 import android.widget.*;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
-
+    String[] items={"젤리빈","킷캣","롤리팝"};//1차원 배열을 만들기
+    Button but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        Button but=(Button)findViewById(R.id.but_dialog);//대화상자를 나타내는 버튼의 아이디를 통해 불러옴
+        but=(Button)findViewById(R.id.but_dialog);//대화상자를 나타내는 버튼의 아이디를 통해 불러옴
         but.setOnClickListener(this);//현재 이벤트 핸들러 클래스의 객체를 this라는 키워드로 대신함
 
     }
 
     @Override
     public void onClick(View v) {
+
         AlertDialog.Builder dialog=new AlertDialog.Builder(this);//액티비티의 문맥을 말하는것이다.-> 현재 액티비티 반환받는것 !!
         // 만약 this를 쓸 경우 현재 액티비티를 말하는 것
         dialog.setTitle("안녕 한빈!");
-        dialog.setMessage("다이어트하자 살을빼자 밀가루끊자!");
+        dialog.setItems(items, new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                but.setText(items[which]);
+            }
+        });
         dialog.setIcon(R.drawable.first_icon);
         dialog.setPositiveButton("화긴!", new DialogInterface.OnClickListener() {
             @Override
